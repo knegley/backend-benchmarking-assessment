070563f245c67388f22331f00b88a3ccf2e15385
Kyle Negley <kpnegley@gmail.com>
2020-06-16 12:57:23 -0400

    refactored code to get tests to work for find anagrams

--- anagrams.py
+++ anagrams.py
@@ -10,7 +10,7 @@ for an arbitrary list of strings.
 
 # Your name here, and any other people/sources who helped.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Kyle Negley"
 
 import sys
 
@@ -27,11 +27,22 @@ def find_anagrams(words):
     Example:
     {'dgo': ['dog'], 'act': ['cat', 'act']}
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
+    # anagrams_test = {}
+    # for word in words:
+    #     anagrams_test.setdefault(alphabetize(word), [])
+
+    anagrams = {alphabetize(word): [] for word in words}
+
+    print(anagrams)
+    for word in words:
+        if alphabetize(word) in anagrams:
+            anagrams[alphabetize(word)].append(word)
+
+    # anagrams = {
+    #     alphabetize(word): [
+    #         w for w in words
+    #         if alphabetize(w) == alphabetize(word)]
+    #     for word in words}
     return anagrams
 
 
